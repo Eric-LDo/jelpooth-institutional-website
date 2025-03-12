700a9e4b7dccc86820499bc77885a434566d8e1b
Eric-LDo <121363351+Eric-LDo@users.noreply.github.com>
2025-03-12 17:06:08 -0300

    tests

--- jelpooth-institutional-website/src/app/page.tsx
+++ jelpooth-institutional-website/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Card from "./components/card";
 
 export default function Home() {
   return (
     <div className="font-sans">
-      website works!
+      <section className="bg-red-500 w-screen h-screen">
+
+      </section>
+      <section className="bg-blue-500 w-screen h-screen flex justify-center items-center">
+        <Card/>
+      </section>
+      <section className="bg-green-500 w-screen h-screen">
+
+      </section>
     </div>
   );
 }
